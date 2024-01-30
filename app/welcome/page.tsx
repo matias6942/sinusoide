e31f21c3d28d46092bdb561923a28914904e8771
matias_6942 <matias.zamora@protonmail.com>
2024-01-29 21:38:22 -0300

    KAN-17-<welcome-message> "root layout and titles set for password, signin and welcome routes"

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Hola!',
+}
+
 export default function Page() {
     return <h1>Hello, this is the Welcome Page!</h1>
 }
